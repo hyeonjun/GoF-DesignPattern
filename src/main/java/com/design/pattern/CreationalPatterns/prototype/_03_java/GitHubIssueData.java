20cf3b88d3e5f05c30ceec8dea62551230a7b005
hjju <hjju@fin2b.com>
2023-02-17 11:07:41 +0900

    Java, Spring의 프로토타입, 얕은 복사 예시

---
+++ src/main/java/com/design/pattern/CreationalPatterns/prototype/_03_java/GitHubIssueData.java
@@ -0,0 +1,17 @@
+package com.design.pattern.CreationalPatterns.prototype._03_java;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GitHubIssueData {
+
+  private int id;
+  private String title;
+  private String repositoryUser;
+  private String repositoryName;
+
+}
