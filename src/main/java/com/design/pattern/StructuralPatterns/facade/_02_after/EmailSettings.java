bb2a88e0fde1192256a0b8e76613330b36b33923
hjju <hjju@fin2b.com>
2023-03-05 18:14:21 +0900

    퍼사드 패턴 적용 후 - 결국 이전에 Client가 가지고 있던 의존성을 뒤쪽에서 다 가지게 되었지만, Client가 하나가 아닐 경우 더욱 좋은 패턴이 될 것

---
+++ src/main/java/com/design/pattern/StructuralPatterns/facade/_02_after/EmailSettings.java
@@ -0,0 +1,14 @@
+package com.design.pattern.StructuralPatterns.facade._02_after;
+
+public class EmailSettings {
+
+  private String host;
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+}
