8beb4eed766caae8d2e2ed780df987b4099ba17c
hjju <hjju@fin2b.com>
2023-03-05 19:29:42 +0900

    프록시 패턴 적용 후 - lazy initialization

--- src/main/java/com/design/pattern/StructuralPatterns/proxy/_02_after/Client.java
+++ src/main/java/com/design/pattern/StructuralPatterns/proxy/_02_after/Client.java
@@ -3,7 +3,7 @@ package com.design.pattern.StructuralPatterns.proxy._02_after;
 public class Client {
 
   public static void main(String[] args) throws InterruptedException {
-    GameService gameService = new GameServiceProxy(new DefaultGameService());
+    GameService gameService = new GameServiceProxy();
     gameService.startGame();
   }
 }
--- src/main/java/com/design/pattern/StructuralPatterns/proxy/_02_after/GameServiceProxy.java
+++ src/main/java/com/design/pattern/StructuralPatterns/proxy/_02_after/GameServiceProxy.java
@@ -4,13 +4,12 @@ public class GameServiceProxy implements GameService {
 
   private GameService gameService;
 
-  public GameServiceProxy(GameService gameService) {
-    this.gameService = gameService;
-  }
-
   @Override
   public void startGame() {
     long before = System.currentTimeMillis();
+    if (this.gameService == null) { // lazy initialization
+      this.gameService = new DefaultGameService();
+    }
     gameService.startGame();
     System.out.println(System.currentTimeMillis() - before);
   }
